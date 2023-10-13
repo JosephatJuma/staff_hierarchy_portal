06858f950b74db492d2ef7eb30927ddabda3d677
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-10-13 09:38:19 +0300

    Delete unused files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Welcome from "./Welcome";
 import MainLayout from "./MainLayout";
-import Users from "./users/Users";
 import Home from "./home/Home";
 import Staff from "./staff/Staff";
 function App() {
@@ -10,10 +8,8 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          {/* <Route path="/" element={<Welcome />} /> */}
           <Route path="/" element={<MainLayout />}>
             <Route index element={<Home />} />
-            <Route path="users" element={<Users />} />
             <Route path="staff" element={<Staff />} />
           </Route>
         </Routes>
--- src/users/Users.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Users() {
-  return <div>Users List</div>;
-}
-
-export default Users;
