fa5bcb1b1cdd5af229e1fc99517a3432409d8b84
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-10-13 11:22:29 +0300

    Add condional rendering on Staff Component

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import StaffHierarchy from "./StaffHierarchy";
+import { useSelector } from "react-redux";
+import LoadingComponent from "../components/LoadingComponent";
 
 function Home() {
-  return (
-    <div>
-      <StaffHierarchy />
-    </div>
-  );
+  const loading = useSelector((state) => state.staff.loading);
+
+  const loadingRender = {
+    true: <LoadingComponent />,
+    false: <StaffHierarchy />,
+  }[loading];
+  return <div>{loadingRender}</div>;
 }
 
 export default Home;
--- src/staff/Staff.jsx
+++ src/staff/Staff.jsx
@@ -2,16 +2,23 @@ import React from "react";
 import { Grid } from "@mui/material";
 import { useSelector } from "react-redux";
 import StaffCard from "../components/StaffCard";
+import LoadingComponent from "../components/LoadingComponent";
 
 function Staff() {
   const userList = useSelector((state) => state.staff.userList);
-  return (
-    <Grid container spacing={2}>
-      {userList.map((staff) => (
-        <StaffCard key={staff.id} staff={staff} />
-      ))}
-    </Grid>
-  );
+  const loading = useSelector((state) => state.staff.loading);
+
+  const loadingRender = {
+    true: <LoadingComponent />,
+    false: (
+      <Grid container spacing={2}>
+        {userList.map((staff) => (
+          <StaffCard key={staff.id} staff={staff} />
+        ))}
+      </Grid>
+    ),
+  }[loading];
+  return <>{loadingRender}</>;
 }
 
 export default Staff;
